6ee2a6565b02348d4d7d441d617b3f5edc4df7f3
i3ook <i-non@hotmail.com>
2019-05-15 13:49:10 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'SjDk2XijNLa1F6PXB3gl+LmoVGeqKlAB8doxO/kj7ab6JPimuc4mQPQBa8qOOnfDnukrL/gfTJD/5N04P1FHY9nBrLcKU26q99I85aE3hEf/Ru2u4Bi1HdhXvF5gY/deFBwRt3fTxobfqWVNcOzDegdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
